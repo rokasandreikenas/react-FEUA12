5c94080e18af3bc8d00f700350007b90e8109cf0
rokasandreikenas <shyptt@gmail.com>
2024-03-19 19:09:58 +0200

    Add react router example

---
+++ 8_paskaita/src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
+import AboutUs from "./pages/AboutUs";
+
+const App = () => {
+  return (
+    <>
+      <nav
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 16,
+          gap: 32,
+        }}
+      >
+        <Link to="/">Home</Link>
+        <Link to="/contacts">Contacts</Link>
+        <Link to="/about-us">About us</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
