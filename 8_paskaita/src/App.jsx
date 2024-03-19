ab57433d35933bbae60474d245776690eb316cd2
rokasandreikenas <shyptt@gmail.com>
2024-03-19 19:14:47 +0200

    Add folder structure example

--- 8_paskaita/src/App.jsx
+++ 8_paskaita/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 import Contacts from "./pages/Contacts";
 import AboutUs from "./pages/AboutUs";
 
---
+++ 8_paskaita/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import HomeSection from "./HomeSection";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <HomeSection />
+    </div>
+  );
+};
+
+export default Home;
